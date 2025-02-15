b8e3b41faa4ac2270f8ee994d85d98cdb7e03a52
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-15 20:54:44 -0300

    Image component logo

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,3 +1,5 @@
+import Logo from "@/components/ui/Logo";
+
 export default function RootLayout ({
     children,
 }: Readonly<{
@@ -6,10 +8,13 @@ export default function RootLayout ({
     return (
         <>
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
-                <div className="bg-purple-950"></div>
-                <div className="p-10 lg:py-28">
+                <div className="bg-purple-950 flex justify-center">
+                    <div className="w-96 py-10 lg:py-20">
+                        <Logo />
+                    </div>
+                </div>
+                <div className="p-10 lg:py-28 ">
                     <div className="max-w-3xl mx-auto">
-
                         {children}
                     </div>
                 </div>
