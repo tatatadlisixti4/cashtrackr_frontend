8ba3512d636622ad0df41147360166958c3100c2
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-15 21:01:42 -0300

    Bg personalizado con tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "auth": "url('/grafico.svg')",
+      },
+      backgroundSize: {
+        "30": "30rem"
+      }
+    },
   },
   plugins: [],
 };
