ef5ab3059e824857eb21d00b3fba776d3c2743dc
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-18 13:00:26 -0300

    Enviando petición hacia el backend y retornando errores para su renderizado en un toast

--- actions/authenticate-user.action.ts
+++ actions/authenticate-user.action.ts
@@ -1,5 +1,5 @@
 "use server"
-import {LoginSchema} from "@/src/schemas"
+import {ErrorResponseSchema, LoginSchema, SuccessSchema} from "@/src/schemas"
 
 type ActionStateType = {
     errors: string[]
@@ -19,6 +19,30 @@ export async function authenticate(prevState: ActionStateType, formData: FormDat
             errors: auth.error.errors.map(error =>  error.message)
         }
     }  
+
+    const url = `${process.env.API_URL}/auth/login`
+    const req = await fetch(url, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: auth.data.email,
+            password: auth.data.password
+        })
+    })
+    const json = await req.json()
+
+    if(!req.ok) {
+        const {error} = ErrorResponseSchema.parse(json)
+        return {
+            errors: [error]
+        }
+    }
+
+    const token = SuccessSchema.parse(json)
+    console.log(token)
+    
     return {
         errors: []
     }
