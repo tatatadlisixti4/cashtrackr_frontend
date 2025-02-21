3630ba5e22d72b50754df1dfe8d89474818222ee
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-21 18:28:47 -0300

    Not found para budgets y validando presupuesto con zod

--- app/admin/budgets/[id]/edit/page.tsx
+++ app/admin/budgets/[id]/edit/page.tsx
@@ -1,4 +1,8 @@
 import getToken from "@/src/auth/token"
+import {BudgetAPIResponseSchema} from "@/src/schemas"
+import Link from "next/link"
+import {notFound} from "next/navigation"
+
 async function getBudgetById(budgetId: string) {
     const token = getToken()
     const url = `${process.env.API_URL}/budgets/${budgetId}`
@@ -8,13 +12,40 @@ async function getBudgetById(budgetId: string) {
         }
     })
     const json = await req.json()
-    console.log(json)
+    if(!req.ok) {
+        notFound()
+        
+    } 
+    const budget = BudgetAPIResponseSchema.safeParse(json)
+    if(!budget.success) {
+        notFound()
+    }
+    return budget.data
 }   
 
 export default async function EditBudgetPage({params}: {params : {id: string}}) {
-    const {id} = params
-    getBudgetById(id)
+    const budget = await getBudgetById(params.id)
     return (
-        <div>EditBudgetPage</div>
+        <>
+            <div className='flex flex-col-reverse md:flex-row md:justify-between items-center'>
+                <div className='w-full md:w-auto'>
+                    <h1 className='font-black text-4xl text-purple-950 my-5'>
+                        Editar Presupuesto: {budget.name}
+                    </h1>
+                    <p className="text-xl font-bold">Llena el formulario y crea un nuevo {''}
+                        <span className="text-amber-500">presupuesto</span>
+                    </p>
+                </div>
+                <Link
+                    href={'/admin'}
+                    className='bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center'
+                >
+                    Volver
+                </Link>
+            </div>
+            <div className='p-10 mt-10  shadow-lg border '>
+
+            </div>
+        </>
     )
 }
