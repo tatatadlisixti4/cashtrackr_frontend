28c6df70d7000bb72a71636b068bc9b188ccf1d7
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-23 17:02:43 -0300

    .

--- app/admin/budgets/[id]/edit/page.tsx
+++ app/admin/budgets/[id]/edit/page.tsx
@@ -1,30 +1,8 @@
-import EditBudgetForm from "@/components/budgets/EditBudgetForm"
-import getToken from "@/src/auth/token"
-import {BudgetAPIResponseSchema} from "@/src/schemas"
-import { Metadata } from "next"
+import {Metadata} from "next"
 import Link from "next/link"
-import {notFound} from "next/navigation"
+import EditBudgetForm from "@/components/budgets/EditBudgetForm"
+import {getBudgetById} from "@/src/services/budgets"
 
-
-async function getBudgetById(budgetId: string) {
-    const token = getToken()
-    const url = `${process.env.API_URL}/budgets/${budgetId}`
-    const req = await fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    })
-    const json = await req.json()
-    if(!req.ok) {
-        notFound()
-        
-    } 
-    const budget = BudgetAPIResponseSchema.safeParse(json)
-    if(!budget.success) {
-        notFound()
-    }
-    return budget.data
-}   
 
 export async function generateMetadata({params} : {params: {id: string}}) : Promise<Metadata>{
     const budget = await getBudgetById(params.id)
---
+++ src/services/budgets.ts
@@ -0,0 +1,24 @@
+import {notFound} from "next/navigation"
+import getToken from "@/src/auth/token"
+import {BudgetAPIResponseSchema} from "@/src/schemas"
+
+import {cache} from "react"
+export const getBudgetById = cache(async (budgetId: string) => {
+    const token = getToken()
+    const url = `${process.env.API_URL}/budgets/${budgetId}`
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    const json = await req.json()
+    if(!req.ok) {
+        notFound()
+        
+    } 
+    const budget = BudgetAPIResponseSchema.safeParse(json)
+    if(!budget.success) {
+        notFound()
+    }
+    return budget.data
+})
