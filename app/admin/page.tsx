5024248ef629e005e43045c54d1b650bbdb66d28
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-23 21:01:06 -0300

    modal listo, falta editar el contenido

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,7 @@ import {BudgetsAPIResponseSchema} from "@/src/schemas"
 import getToken from "@/src/auth/token"
 import {formatCurrency, formatDate} from "@/utils"
 import BudgetMenu from "@/components/budgets/BudgetMenu"
+import DeleteBudgetModal from "@/components/budgets/DeleteBudgetModal"
 
 export const metadata: Metadata = {
     title: "CashTrackr - Panel de Administración",
@@ -46,37 +47,39 @@ export default async function AdminPage() {
             </div>
 
             {budgets.length ? (
-                <ul role="list" className="divide-y divide-gray-300 border shadow-lg mt-10 ">
-                {budgets.map((budget) => (
-                    <li key={budget.id} className="flex justify-between gap-x-6 p-5 ">
-                        <div className="flex min-w-0 gap-x-4">
-                            <div className="min-w-0 flex-auto space-y-2">
-                                <p className="text-sm font-semibold leading-6 text-gray-900">
-                                    <Link
-                                        className="hover:underline text-2xl font-bold"
-                                        href={`admin/budgets/${budget.id}`}
-                                    >
-                                        {budget.name}
-                                    </Link>
-                                </p>
-                                <p className="text-xl font-bold text-amber-500">
-                                    {formatCurrency(+budget.amount)}
-                                </p>
-                                <p className='text-gray-500  text-sm'>
-                                    Última Actualización: {''}
-                                    <span className="font-bold">{formatDate(budget.updatedAt)}</span>
+                <>
+                    <ul role="list" className="divide-y divide-gray-300 border shadow-lg mt-10 ">
+                    {budgets.map((budget) => (
+                        <li key={budget.id} className="flex justify-between gap-x-6 p-5 ">
+                            <div className="flex min-w-0 gap-x-4">
+                                <div className="min-w-0 flex-auto space-y-2">
+                                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                                        <Link
+                                            className="hover:underline text-2xl font-bold"
+                                            href={`admin/budgets/${budget.id}`}
+                                        >
+                                            {budget.name}
+                                        </Link>
+                                    </p>
+                                    <p className="text-xl font-bold text-amber-500">
+                                        {formatCurrency(+budget.amount)}
+                                    </p>
+                                    <p className='text-gray-500  text-sm'>
+                                        Última Actualización: {''}
+                                        <span className="font-bold">{formatDate(budget.updatedAt)}</span>
 
-                                </p>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="flex shrink-0 items-center gap-x-6">
-                            <BudgetMenu budgetId={budget.id} />
-                        </div>
-                    </li>
-                    ))}
-                </ul>
-                
+                            <div className="flex shrink-0 items-center gap-x-6">
+                                <BudgetMenu budgetId={budget.id} />
+                            </div>
+                        </li>
+                        ))}
+                    </ul>
+                    <DeleteBudgetModal />    
+                </>
             ) : (
                 <p className="text-center py-20">
                     No hay presupuestos aún. {''}
--- components/budgets/BudgetMenu.tsx
+++ components/budgets/BudgetMenu.tsx
@@ -4,8 +4,10 @@ import Link from "next/link"
 import {Menu, MenuButton, MenuItem, MenuItems, Transition} from "@headlessui/react"
 import {EllipsisVerticalIcon} from "@heroicons/react/20/solid"
 import {Budget} from "@/src/types"
+import { useRouter } from "next/navigation"
 
 export default function BudgetMenu({budgetId}: {budgetId: Budget['id']}) {
+    const router = useRouter()
     return (
         <>
             <Menu as="div" className="relative flex-none">
@@ -44,7 +46,7 @@ export default function BudgetMenu({budgetId}: {budgetId: Budget['id']}) {
                             <button
                                 type='button'
                                 className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                onClick={() => {}}
+                                onClick={() => router.push(`?deleteBudgetId=${budgetId}`)}
                             >
                                 Eliminar Presupuesto
                             </button>
