39cd521cde13a6f59e34dd0e987b49e0eebd424d
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-25 12:42:18 -0300

    Consultando la api del backend en la api del frontend con route handler - api route

--- app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
+++ app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,6 +1,23 @@
 import {verifySession} from "@/src/auth/dal"
-
-export async function GET() {
-    await verifySession()
-    return Response.json('holiwis')
+import getToken from "@/src/auth/token"
+type paramsType = {
+    budgetId: string,
+    expenseId: string
 }
+export async function GET(request: Request, {params}: {params: paramsType}) {
+    await verifySession()
+    const token = getToken()
+    const url = `${process.env.API_URL}/budgets/${params.budgetId}/expenses/${params.expenseId}`
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    const json = await req.json()
+    if(!req.ok) {
+        return Response.json(json.error, {status: 403})
+    }
+
+    return Response.json(json)
+}
+
