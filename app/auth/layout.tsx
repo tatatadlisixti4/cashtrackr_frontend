b39c61681927c6eedeaa27cbf2f20f7c9c746f0e
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-27 19:25:50 -0300

    Listo

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/ui/Logo";
 import ToastNotification from "@/components/ui/ToastNotification"
+import Link from "next/link";
 
 export default function RootLayout ({
     children,
@@ -11,7 +12,9 @@ export default function RootLayout ({
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
                 <div className="flex justify-center bg-purple-950 lg:bg-auth lg:bg-30 bg-no-repeat bg-left-bottom">
                     <div className="w-96 py-10 lg:py-20">
-                        <Logo />
+                        <Link href="/">
+                            <Logo />
+                        </Link>
                     </div>
                 </div>
                 <div className="p-10 lg:py-28 ">
